a0cb506d50d708a577f56eb0ede539a966f0167f
sandilokaa <sandyloka790@gmail.com>
2024-10-24 14:32:20 +0700

    docs: integrate module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './modules/users/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/you-app-db'),
+    UserModule,
+  ],
 })
 export class AppModule {}
