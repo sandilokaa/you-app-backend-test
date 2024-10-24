30db7218928b410f36460d2496ae4c0411482c0c
sandilokaa <sandyloka790@gmail.com>
2024-10-24 14:32:59 +0700

    docs: next user

---
+++ src/modules/users/interface/user.interface.ts
@@ -0,0 +1,12 @@
+export interface User {
+  name: string;
+  email: string;
+  username: string;
+  password: string;
+  gender: string;
+  birthday: Date;
+  horoscope: string;
+  zodiac: string;
+  height: string;
+  weight: string;
+}
